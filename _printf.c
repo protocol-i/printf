cdccc51d70dffb5925a22226941571ddabf9d36c
protocol-i <protocoliddriss@gmail.com>
2023-08-20 06:55:51 -0700

    separete function files and added a struct file

--- _printf.c
+++ _printf.c
@@ -1,69 +1,6 @@
 #include "main.h"
 
 /**
- * _size - Calculate the number of digits in a positive integer.
- * @nb: The unsigned integer for which to calculate the size.
- *
- * Return: The size (number of digits) of the integer.
- */
-int _size(unsigned int nb)
-{
-	int size = 0;
-
-	while (nb > 0)
-	{
-		size++;
-		nb = nb / 10;
-	}
-	return (size);
-}
-
-/**
- * _putnbr - Print an integer to the standard output.
- * @n: The integer to be printed.
- *
- * Return: The number of characters printed.
- */
-int _putnbr(int n)
-{
-	char *number;
-	unsigned int nbr;
-	int len = 0;
-	int size;
-
-	if (n == 0)
-		return (write(1, "0", 1));
-	if (n < 0)
-	{
-		len++;
-		write(1, "-", 1);
-		nbr = n * (-1);
-	}
-	else
-		nbr = n;
-	size = _size(nbr);
-	len += size;
-	number = (char *)malloc((size + 1) * sizeof(char));
-	if (!number)
-		return (0);
-	number[size--] = '\0';
-	while (nbr > 0)
-	{
-		number[size--] = (nbr % 10) + '0';
-		nbr = nbr / 10;
-	}
-	size = 0;
-	while (number[size])
-	{
-		write(1, &number[size], 1);
-		size++;
-	}
-	free(number);
-	number = NULL;
-	return (len);
-}
-
-/**
  *_printf - prints formatted output to the standard output.
  * @format: format string.
  * @...: a variable number of arguments to be printed.
--- main.h
+++ main.h
@@ -5,6 +5,15 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct printf_utils {
+	int (*_size)(unsigned int);
+	int (*_putnbr)(int);
+	int (*_printf)(const char *, ...);
+} PrintfUtils;
+
+int _size(unsigned int nb);
+int _putnbr(int n);
 int _printf(const char *format, ...);
 
 #endif
