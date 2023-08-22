3d5358a0c047154b7e0a53b5dacfa1414236b2d9
faouziaen <136031053+faouziaen@users.noreply.github.com>
2023-08-22 01:34:35 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -74,48 +74,37 @@ int _printf(const char *format, ...)
 	va_list arg_list;
 	int cha_print = 0;
 
-	if (format == NULL)
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-
 	va_start(arg_list, format);
 	while (*format)
 	{
 		if (*format != '%')
-		{
 			cha_print += write(1, format, 1);
-			format++;
-			continue;
-		}
+		else if (*format == '%' && (*(format + 1) == ' ' || *(format + 1) == '\0'))
+				return (-1);
+		else
+		{
+			format++;	
+			if (*format == '%')
+				cha_print += write(1, "%", 1);
+			else if (*format == 'c')
+			{
+				char c  = va_arg(arg_list, int);
 
-		format++;
-		if (*format == '\0') /* Handle trailing '%' */
-		{
-			cha_print += write(1, "%", 1);
-			break;
-		}
-		else if (*format == 'c')
-		{
-			char c = va_arg(arg_list, int);
+				cha_print += write(1, &c, 1);
+			}
+			else if (*format == 's')
+			{
+				char *strg = va_arg(arg_list, char*);
 
-			cha_print += write(1, &c, 1);
-		}
-		else if (*format == 's')
-		{
-			char *strg = va_arg(arg_list, char*);
-
-			if (strg == NULL)
-				strg = "(null)";
-			int str_len = strlen(strg);
-
-			cha_print += write(1, strg, str_len);
-		}
-		else if (*format == 'd' || *format == 'i')
-		{
-			cha_print += _putnbr(va_arg(arg_list, int));
-		}
-		else if (*format == '%')
-		{
-			cha_print += write(1, "%", 1);
+				if (strg == NULL)
+					strg = "(null)";
+				while (*strg != '\0')
+					cha_print += write(1, strg++, 1);
+			}
+			else if (*format == 'd' || *format == 'i')
+				cha_print += _putnbr(va_arg(arg_list, int));
 		}
 		format++;
 	}
