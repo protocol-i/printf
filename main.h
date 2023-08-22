ca47ba26a35caaa40b7f79d0d7228d1f30d54620
protocol-i <protocoliddriss@gmail.com>
2023-08-22 03:12:27 -0700

    restore code as it was

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
+
+int _size(unsigned int nb);
+int _putnbr(int n);
+int _printf(const char *format, ...);
+
+#endif
