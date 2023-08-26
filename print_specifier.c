a32f965e17957da4a3542eab71574d114a50ad07
protocol-i <protocoliddriss@gmail.com>
2023-08-25 19:29:49 -0700

    correcting betty errors

--- print_specifier.c
+++ print_specifier.c
@@ -1,52 +1,38 @@
 #include "main.h"
+
 /**
  * print_specifier - Handle specific specifiers.
- * @specifier: The specifier character.
- * @arg_list: The va_list of arguments.
+ * @s: The specifier character.
+ * @a: The va_list of arguments.
  *
  * Return: Number of characters printed.
  */
 int print_specifier(char s, va_list a)
 {
-	int p = 0;
+	int p = 0, n;
+	unsigned int tmp;
 	char *t;
-	int n;
-	unsigned int u, x, o, X;
 
-	switch (s)
-	{
-		case 'c':
-			p += _putchar(va_arg(a, int));
-			break;
-		case 's':
-			t = va_arg(a, char *);
-			p += t ? _putstr(t) : _putstr("(null)");
-			break;
-		case 'd': case 'i':
-			p += _putnbr(n = va_arg(a, int));
-			break;
-		case 'b':
-			p += _putnbr_binary(va_arg(a, unsigned int));
-			break;
-		case '%':
-			p += _putchar('%');
-			break;
-		case 'u':
-			p += print_unsigned(u = va_arg(a, unsigned int));
-			break;
-		case 'x':
-			p += print_hex_lower(x = va_arg(a, unsigned int));
-			break;
-		case 'o':
-			p += print_octal(o = va_arg(a, unsigned int));
-			break;
-		case 'X':
-			p += print_hex_upper(X = va_arg(a, unsigned int));
-			break;
-		default:
-			p += _putchar('%');
-			p += _putchar(s);
-	}
+	if (s == 'c')
+		p += _putchar(va_arg(a, int));
+	else if (s == 's')
+		p += (t = va_arg(a, char *)) ? _putstr(t) : _putstr("(null)");
+	else if (s == 'd' || s == 'i')
+		p += _putnbr(n = va_arg(a, int));
+	else if (s == 'b')
+		p += _putnbr_binary(va_arg(a, unsigned int));
+	else if (s == '%')
+		p += _putchar('%');
+	else if (s == 'u')
+		p += (tmp = va_arg(a, unsigned int), print_unsigned(tmp));
+	else if (s == 'x')
+		p += (tmp = va_arg(a, unsigned int), print_hex_lower(tmp));
+	else if (s == 'o')
+		p += (tmp = va_arg(a, unsigned int), print_octal(tmp));
+	else if (s == 'X')
+		p += (tmp = va_arg(a, unsigned int), print_hex_upper(tmp));
+	else
+		p += _putchar('%') + _putchar(s);
 
 	return (p);
 }
